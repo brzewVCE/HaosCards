e1e35c008dd68f8e41d324d37c6b060113098f38
PrzemChuck <przemchuck.biznes@gmail.com>
2023-11-03 19:03:14 +0100

    Updated classes.py

--- HaosCards/classes.py
+++ HaosCards/classes.py
@@ -1,5 +1,6 @@
 import random
 import string
+import json
 
 def generate_gamecode(length=6):
     characters = string.digits
@@ -9,13 +10,18 @@ def generate_gamecode(length=6):
     return random_string
 
 class Player:
-    def __init__(self, name, status):
+    def __init__(self, name, status, unique_room):
         self.name = name  
         self.score = 0
         self.cards = {}
         self.status = status
-    pass
+        self.unique_room = unique_room
+        self.acknowledgement = False
+        self.lobby_score = 0
+        self.game_score = 0
 
+    def __str__(self):
+        return f"Name: {self.name}, Score: {self.score}, Status: {self.status}, Unique Room: {self.unique_room}, Acknowledgement: {self.acknowledgement}, Lobby Score: {self.lobby_score}, Game Score: {self.game_score}"
 class Game:
     def __init__(self, point2win, gamecode, cards, players): 
         self.usedCards = {}
@@ -25,6 +31,7 @@ class Lobby:
         self.gamecode = gamecode
         self.settings = {'p2w': 10, "rt":30, "cpp": 8}
         self.players = []
+        self.player_data = []
         self.owner = None
     def __str__(self):
         return f'Lobby(gamecode={self.gamecode}, players={self.players}, owner={self.owner}, settings={self.settings})'
