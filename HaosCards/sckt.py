462b5df284a47ed84309f564b8685d6dfc0e7e22
Przemysław Skrzypek <przemchuck.biznes2@gmail.com>
2023-11-10 01:34:17 +0100

    Working on ack functionality, cleaned up some code

--- HaosCards/sckt.py
+++ HaosCards/sckt.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request, jsonify, Response, send_from_
 from flask_socketio import SocketIO, join_room, leave_room, emit, close_room, rooms
 from flask_session import Session
 import json
-import flask
 import random
 from classes import Game, Player, Lobby, generate_gamecode
 from color_print import print_info, print_error, print_warning
 import eventlet
 import random
+import uuid
 
 lobbies={}
 games={}
@@ -36,52 +36,54 @@ def register_events(socketio):
         print(f'{nickname} generated lobby {gamecode}')
         lobbies_as_strings = [str(lobby) for lobby in lobbies]
         print(f'All lobbies: {lobbies_as_strings}')
-        join(nickname=nickname,gamecode=gamecode)
+        join(nickname, gamecode)
 
 
     @socketio.on('join')
     def join(nickname, gamecode):
         session['nickname'] = nickname
         session['room'] = gamecode
-        room = request.sid
+        player_room = str(uuid.uuid4())
+        session['player_room'] = player_room
+        join_room(player_room)
         new_player = Player(name=nickname, 
                             status = 'player',
-                            unique_room = room)
+                            unique_room = player_room)
         if gamecode in lobbies:
             lobbies[gamecode].player_data.append(new_player)
-            print(f"{lobbies[gamecode].player_data}")
+            print_warning(f"{lobbies[gamecode].player_data}")
         join_room(gamecode)
-        print(f'{nickname} joined {room}')
-        print(f"{nickname}'s rooms: {rooms()}")
+        print(f'{nickname} joined {player_room}')
+        print_error(f"{nickname}'s rooms: {rooms()}")
         print(f'{str(new_player)}')
 
     @socketio.on('player_update')
     def player_update(nickname, gamecode, action):
-        if action == 'join':
-            if gamecode in lobbies:
+        if gamecode in lobbies:
+            if action == 'join':
                 join_room(gamecode)
                 if lobbies[gamecode].owner == None:
                     lobbies[gamecode].owner = nickname
                 
                 if nickname not in lobbies[gamecode].players:
                     lobbies[gamecode].players.append(nickname)
-                    
-                print(str(lobbies[gamecode]))
-                data_update(gamecode)
-        if action == 'leave':
-            if gamecode in lobbies:
-                lobbies[gamecode].players.remove(nickname)
-                if lobbies[gamecode].owner == nickname:
-                    if len(lobbies[gamecode].players) != 0:
-                        lobbies[gamecode].owner = random.choice(lobbies[gamecode].players)
-                    else:
-                        lobbies.pop(gamecode)
-                        close_room(gamecode)
-                        print(f'{nickname} left {gamecode}, deleting it')
-                        return leave_lobby(gamecode, nickname)
-                leave_lobby(gamecode, nickname)
-                print(f'All lobbies: {str(lobbies)}')
-                data_update(gamecode)
+                print(f'{nickname} joined {gamecode}')
+            
+            if action == 'leave':
+                if gamecode in lobbies:
+                    lobbies[gamecode].players.remove(nickname)
+                    if lobbies[gamecode].owner == nickname:
+                        if len(lobbies[gamecode].players) != 0:
+                            lobbies[gamecode].owner = random.choice(lobbies[gamecode].players)
+                        else:
+                            lobbies.pop(gamecode)
+                            close_room(gamecode)
+                            print(f'{nickname} left {gamecode}, deleting it')
+                            return leave_lobby(gamecode, nickname)
+                        leave_lobby(gamecode, nickname)
+
+        print(f'All lobbies: {str(lobbies)}')
+        data_update(gamecode)
 
 
     def leave_lobby(gamecode, nickname):
@@ -96,13 +98,15 @@ def register_events(socketio):
     def data_update(gamecode):
         players = lobbies[gamecode].players
         owner = lobbies[gamecode].owner
-        emit('data_update', {'players': players, 'owner':owner}, to=gamecode)
+        data = {'players': players, 'owner':owner}
+        emit('data_update', data, to=gamecode)
         print(f'Updated with data {players} {owner}')
 
     @socketio.on('request_settings')
     def request_settings(gamecode):
         settings = lobbies[gamecode].settings
-        send_settings(gamecode,settings)
+        send_data("settings_data", settings, gamecode)
+        # send_settings(gamecode,settings)
        
     @socketio.on('change_settings')
     def change_settings(gamecode, action, target):
@@ -115,18 +119,32 @@ def register_events(socketio):
             settings[target] += x
         else:
             settings[target] -= x
-        send_settings(gamecode, settings)
-
+        send_data('settings_data', settings, gamecode)
+        #tu zmiana
 
     def send_settings(gamecode,settings):
         emit('settings_data', settings, to=gamecode)
 
-    #make a callback function for this
-    def ack(function_name, data, gamecode):
-        id = random.randint(0, 1000000000)
-        acknowledgements[id] = False
-        emit(function_name, data, to=gamecode, callback=acknowledgements(id))
 
+    def send_data(function_name, data, gamecode):
+        players = lobbies[gamecode].player_data
+        for player in players:
+            player_room = player.unique_room
+            session_id = request.sid
+            print_error(f"session_id: {session_id}")
+            acknowledgement_id = str(uuid.uuid4())
+            acknowledgements[acknowledgement_id] = False
+            data['acknowledgement_id'] = acknowledgement_id
+            print_warning(f"data: {data}")
+            while acknowledgements[acknowledgement_id] != True:
+                emit(function_name, data, to=player_room)
+                print_info(f"Sent {function_name} to {player_room}")
+                print_info(f"{acknowledgement_id} {acknowledgements[acknowledgement_id]}")
+                eventlet.sleep(0.1)
+            
 
-
+    @socketio.on('acknowledge')
+    def acknowledge(id):
+        acknowledgements[id] = True
+        print_info(f"ACKNOWLEDGED {id}")
 
